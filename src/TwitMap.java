c5a1cdab866182966dda6fa4348bb1a5c5bf2fe6
Annapurna Vemuri <annapurna.vmss@gmail.com>
2014-10-29 21:44:03 -0400

    Remove HelloWorld

--- src/TwitMap.java
+++ src/TwitMap.java
@@ -1,15 +1,34 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * Created by annapurna on 10/29/14.
  */
-public class TwitMap {
+public class TwitMap  extends HttpServlet {
+  private String message;
   TweetGet getTweets;
 
-  public TwitMap() {
-    init();
+  public void init() {
+    getTweets = new TweetGet();
+    message = "Hello!";
   }
 
-  private void init() {
-    getTweets = new TweetGet();
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    // Set response content type
+    response.setContentType("text/html");
+
+    // Actual logic goes here.
+    PrintWriter out = response.getWriter();
+    out.println("<h1>" + message + "</h1>");
+  }
+
+  public void destroy() {
+    // do nothing.
   }
 
 }
--- src/TwitMapAsyncServlet.java
+++ src/TwitMapAsyncServlet.java
@@ -5,19 +5,14 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
-@WebServlet(urlPatterns={"/simplesse"})
-public class SSEAsyncServlet extends HttpServlet {
+@WebServlet(urlPatterns={"/twitmap"})
+public class TwitMapAsyncServlet extends HttpServlet {
 
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	@Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res)
             throws IOException, ServletException {
-
-        // set content type
         res.setContentType("text/event-stream");
         res.setCharacterEncoding("UTF-8");
 
